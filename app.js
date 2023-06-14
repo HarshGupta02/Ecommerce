008d162ece9b805fded3f874bc57abd79b2d2138
Harsh Gupta <simpcoder02@gmail.com>
2023-06-14 13:06:38 +0530

    Console Log 5

--- app.js
+++ app.js
@@ -3,24 +3,17 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-// const errorMiddleware = require("../backend/middleware/error");
 const errorMiddleware = require("./error.js");
 const dotenv = require("dotenv");
 const path = require("path");
 const BASE_URL = process.env.BASE_URL;
 
-// dotenv.config({path : "backend/config/config.env"});
 dotenv.config({path : "./config.env"});
 
 app.use(express.json({limit : '50mb'}));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended : true, limit : '50mb'}));
 app.use(fileUpload());
-
-// const product = require("../backend/routes/productRoute");
-// const user = require("../backend/routes/userRoute");
-// const order = require("../backend/routes/orderRoutes");
-// const payment = require("../backend/routes/paymentRoute");
 
 const product = require("./productRoute.js");
 const user = require("./userRoute.js");
@@ -32,11 +25,11 @@ app.use(`${BASE_URL}/api/v1`, user);
 app.use(`${BASE_URL}/api/v1`, order);
 app.use(`${BASE_URL}/api/v1`, payment);
 
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
-// })
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+})
 
 app.use(errorMiddleware);
 
