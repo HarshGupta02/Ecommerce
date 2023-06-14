6d6b493ec25f5622c13aabe0f07ac090e08d7427
Harsh Gupta <simpcoder02@gmail.com>
2023-06-14 11:22:43 +0530

    Log prints

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
+console.log(path.join(__dirname, "../frontend/build"));
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
