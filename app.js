00c186f4357bd819b395ec475e41854934025c9c
Harsh Gupta <simpcoder02@gmail.com>
2023-06-14 13:00:14 +0530

    Console Log 3

--- app.js
+++ app.js
@@ -32,16 +32,11 @@ app.use(`${BASE_URL}/api/v1`, user);
 app.use(`${BASE_URL}/api/v1`, order);
 app.use(`${BASE_URL}/api/v1`, payment);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-console.log(path.join(__dirname, "../frontend/build"));
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+//     res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
 // })
-
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
-})
 
 app.use(errorMiddleware);
 
